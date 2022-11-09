ee7d5924532b6c9b5d2aa6506f7b7ae979f6fa50
farukwebcode21 <farukahmadmk@yahoo.com>
2022-11-09 20:43:30 +0600

    private route complete

--- src/components/Shipment/Shipment.js
+++ src/components/Shipment/Shipment.js
@@ -1,6 +1,5 @@
 import React, { useState } from "react";
 import { useAuthState } from "react-firebase-hooks/auth";
-import { Link } from "react-router-dom";
 import auth from "../../firebase.init";
 
 const Shipment = () => {
